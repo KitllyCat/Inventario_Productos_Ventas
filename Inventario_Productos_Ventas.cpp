4a10553f516b78735d555e5f99e8b2d5afb745cc
KitllyCat <cjchambem@unjbg.edu.pe>
2025-06-26 23:46:33 -0500

    Se añadio el menu principal, faltan todas la funciones correspondientes para su funcionamiento

---
+++ Inventario_Productos_Ventas.cpp
@@ -0,0 +1,99 @@
+/*Inventario de Productos y Ventas
+Objetivo: Desarrollar un programa que permita registrar, listar, buscar, actualizar y eliminar productos, así como gestionar las ventas.
+Requisitos:
+• Define una estructura Producto con los campos: nombre (cadena), precio (flotante).
+• Define una estructura Venta con los campos: idVenta (entero), producto (cadena), cantidad (entero), precioTotal (flotante).
+• Implementa un menú con las siguientes opciones:
+	A: Registrar un nuevo producto.
+	B: Listar los productos registrados.
+	C: Buscar un producto por nombre.
+	D: Actualizar los datos de un producto.
+	E: Eliminar un producto.
+	F: Registrar una venta.
+	G: Listar las ventas realizadas.
+	H: Calcular el total de ventas realizadas.
+	S: Salir del programa.
+• El menú debe estar ciclado y permitir volver al menú principal después de cada opción.*/
+#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+struct Producto{
+    string nombre;
+    float precio;
+};
+struct venta{
+    int idVenta;
+    string producto;
+    int cant;
+    float precioT;
+};
+
+int main(){	
+	SetConsoleOutputCP(CP_UTF8);
+	int opcion;
+	do{
+		cout<<"-----Inventario de Productos y Ventas-----"<<endl<<endl;
+		cout<<"Elija la opcion que desea realizar en el programa:"<<endl;
+		cout<<"1) Registrar un nuevo producto"<<endl;
+		cout<<"2) Listar todos los productos registrados"<<endl;
+		cout<<"3) Buscar un producto por su nombre"<<endl;
+		cout<<"4) Actualizar los datos de un producto"<<endl;
+		cout<<"5) Eliminar un producto"<<endl;
+		cout<<"6) Registrar una nueva venta"<<endl;
+		cout<<"7) Listar las ventas ya realizadas"<<endl;
+		cout<<"8) Calcular el total de ventas realizadas"<<endl;
+		cout<<"9) Salir del programa"<<endl;
+		cout<<"Selecione su opcion: ";
+		cin>>opcion;
+		cout<<endl;
+		
+		switch(opcion){
+			case 1:{
+			//registrarNuevoProducto();
+			break;
+			}
+			case 2:{
+			//listarProductos();
+			break;
+			}
+			case 3:{
+			//buscarProductoNombre();
+			break;
+			}
+			case 4:{
+			//actualizarDatosProducto();
+			break;
+			}
+			case 5:{
+			//eliminarProducto();
+			break;
+        	}
+			case 6:{
+			//registrarNuevaVenta();
+			break;
+			}
+			case 7:{
+			//listarVentasRealizadas();
+			break;
+			}
+			case 8:{
+			//calcularTotalVentas();
+			break;
+			}
+			case 9:{
+				system("cls");
+				cout<<"Saliendo del programa..."<<endl;
+				break;
+			}
+			default:{
+				cout<<"Opción invalida!!! Intente de nuevo..."<<endl;
+				Sleep(1500);
+				system("cls");
+				break;
+			}			
+		}
+	}while(opcion!=9);
+	return 0;
+}
